a0c2c675a37215860f9a855b9e11627fe268729d
isiljic <ivan.siljic.sspzo@gmail.com>
2023-10-31 17:52:50 +0100

    Finnished 2nd task

--- zadaca1/genCylinderObj.cpp
+++ zadaca1/genCylinderObj.cpp
@@ -12,6 +12,12 @@ void genCircleVer(ofstream & circle, double r, double h, int res) {
         double y = r * sin(i * ang * M_PI/180);
 
         circle << "v " << x << " " << y << " " << h << endl;
+    }
+
+    for (int i = 0; i < res; i++) {
+        double x = r * cos(i * ang * M_PI/180);
+        double y = r * sin(i * ang * M_PI/180);
+
         circle << "v " << x << " " << y << " " << h << endl;
     }
 }
@@ -20,25 +26,43 @@ void genCircleNorm(ofstream & circle, double r, int res) {
     double ang = 360.0 / res;
 
     for (int i = 0; i < res; i++) {
+        circle << "vn " << 0 << " " << 0 << " " << 1 << endl;
+    }
+
+    for (int i = 0; i < res; i++) {
         double nx = cos(i * ang * M_PI/180);
         double ny = sin(i * ang * M_PI/180);
 
         circle << "vn " << nx << " " << ny << " " << 0 << endl;
-        circle << "vn " << 0 << " " << 0 << " " << 1 << endl;
     }
 }
 
 void genCircleFace(ofstream & circle, int s, int res) {
-    for (int i = s; i <= res + s; i++) {
-        circle << "f " << s << "//" << s + 1 << " " << i + 1 << "//" << i - s + 3 << " " << i + 2 << "//" << i - s + 4 << endl;
+    for (int i = s; i < res + s - 2; i++) {
+        circle << "f " << s << "//" << s << " " << i + 1 << "//" << i + 1 << " " << i + 2 << "//" << i + 2 << endl;
     }
 }
 
 void genSideFaces(ofstream & cylinder, int res) {
-    for (int i = 1; i <= res; i++) {
-        cylinder << "f " << i << "//" << i << " " << i + res << "//" << i << " " << i + res + 1 << "//" << i + 1 << endl;
-        cylinder << "f " << i + res + 1 << "//" << i + 1 << " " << i << "//" << i << " " << i << "//" << i + 1 << endl;
+    for (int i = 1; i < res; i++) {
+        int n1 = i + res + res/2 + 1;
+        int n2 = i + res*3 + res/2;
+        int n3 = i + res*3 + res/2 + 1;
+        if (n1 > res*2) {
+            n1 -= res;
+        }
+        if (n2 > res*4) {
+            n2 -= res;
+        }
+        if (n3 > res*4) {
+            n3 -= res;
+        }
+        cylinder << "f " << i + res + 1 << "//" << n1 << " " << i + res*3 << "//" << n2 << " " << i + res*3 + 1 << "//" << n3 << endl; // this doesn't work properly
+        cylinder << "f " << i + res*3 << "//" << i + res*3 << " " << i + res << "//" << i + res << " " << i + res + 1 << "//" << i + res + 1 << endl;
     }
+    cylinder << "f " << res + 1 << "//" << res + 1 << " " << res*3 + 1 << "//" << res*3 + 1 << " " << res*4 << "//" << res*4 << endl;
+    cylinder << "f " << res*4 << "//" << res*4 - res/2<< " " << res + 1 << "//" << res + res/2 + 1 << " " << res*2 << "//" << res*2 - res/2 << endl;
+
 }
 
 int main () {
@@ -52,9 +76,11 @@ int main () {
     genCircleVer(cylinder, r, h/2, res);
 
     genCircleNorm(cylinder, r, res);
+    genCircleNorm(cylinder, r, res);
+
 
     genCircleFace(cylinder, 1, res);
-    genCircleFace(cylinder, 37, res);
+    genCircleFace(cylinder, res*2 + 1, res);
 
     genSideFaces(cylinder, res);
 
